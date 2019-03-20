1b78110f02e314089a37d910005376841ff991c3
liuchuanyang <651294311@qq.com>
2019-03-20 14:09:13 +0800

    说明

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="wrap">
-    <p>1、本项目采用了postcss-px-to-viewport插件实现px与vm的转换 在.postcssrc.js文件中进行相关配置</p>
-    <p>2、stylus</p>
-    <p>3、基本组件已全局注册可直接使用</p>
-    <p>4、npm insatll </p>
-    <p>5、运行项目：npm run server 打包项目：npm run build</p>
   </div>
 </template>
 
@@ -26,4 +21,3 @@ export default {
   margin 100px
   text-align left
 </style>
-
